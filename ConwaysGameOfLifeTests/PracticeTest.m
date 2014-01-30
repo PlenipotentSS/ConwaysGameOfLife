957e28c5d418468ec41b91b494eb91381ee5e67c
PlenipotentSS <stevensonhoyt@icloud.com>
2014-01-29 20:44:19 -0800

    attempting to fix test

---
+++ ConwaysGameOfLifeTests/PracticeTest.m
@@ -0,0 +1,34 @@
+//
+//  Test.m
+//  ConwaysGameOfLife
+//
+//  Created by Stevenson on 1/29/14.
+//  Copyright (c) 2014 Steven Stevenson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PracticeTest : XCTestCase
+
+@end
+
+@implementation PracticeTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
